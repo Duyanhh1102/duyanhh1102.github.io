21579933e168e27d514aaa634129fee2bda2418c
Duyanhh1102 <pdanh070728@gmail.com>
2026-02-07 11:05:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const confirmBtn = document.querySelector(".confirm-btn");
+const inviteContent = document.querySelector(".invite-content");
 
 if (confirmBtn) {
   confirmBtn.addEventListener("click", () => {
-    confirmBtn.innerText = "See you soon ✨";
-    confirmBtn.style.pointerEvents = "none";
+    inviteContent.innerHTML = `
+      <p class="line big">Hẹn gặp lại nhé ✨</p>
+      <p class="line">Cảm ơn vì đã dành thời gian cho buổi hẹn này.</p>
+      <p class="line">Chúc bạn một ngày thật dịu dàng.</p>
+    `;
   });
 }
